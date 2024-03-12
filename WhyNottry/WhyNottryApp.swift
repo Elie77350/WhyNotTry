cd3db0ccb20d1385b040518b5cc9c905909212ce
Elie BINGUE <eliebingue7@icloud.com>
2024-03-12 11:30:26 +0100

    Initial Commit

---
+++ WhyNottry/WhyNottryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNottryApp.swift
+//  WhyNottry
+//
+//  Created by Elie BINGUE on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WhyNottryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
